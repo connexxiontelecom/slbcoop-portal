40210f308bb6bfb1a84473de638e6df48956e629
CJ Amanambu <oamanambu@yahoo.com>
2022-08-26 14:51:26 +0100

    modified login failure event

--- app/Controllers/Auth.php
+++ app/Controllers/Auth.php
@@ -1,9 +1,12 @@
 <?php
+
 namespace App\Controllers;
 
-class Auth extends BaseController {
+class Auth extends BaseController
+{
 
-  function login() {
+  function login()
+  {
     if ($this->session->active) {
       return redirect('/');
     }
@@ -11,58 +14,62 @@ class Auth extends BaseController {
     return view('auth/login', $page_data);
   }
 
-  function auth_login() {
+  function auth_login()
+  {
     // @TODO refactor method to use proper Request pattern
     extract($_POST);
     if ($staff_id && $password) {
       $cooperator = $this->cooperatorModel->where('cooperator_staff_id', $staff_id)->first();
       if ($cooperator) {
-      	if (password_verify($password, $cooperator['cooperator_password'])) {
-      		$user_data = array(
+        if (password_verify($password, $cooperator['cooperator_password'])) {
+          $user_data = array(
             'staff_id' => $cooperator['cooperator_staff_id'],
             'firstname' => $cooperator['cooperator_first_name'],
             'lastname' => $cooperator['cooperator_last_name'],
             'othername' => $cooperator['cooperator_other_name'],
-			      'dob' => $cooperator['cooperator_dob'],
-			      'email' => $cooperator['cooperator_email'],
-			      'address' => $cooperator['cooperator_address'],
-			      'city' => $cooperator['cooperator_city'],
-			      'state' => $this->stateModel->where('state_id', $cooperator['cooperator_state_id'])->first(),
-			      'department' => $this->departmentModel->where('department_id', $cooperator['cooperator_department_id'])->first(),
-			      'location' => $this->locationModel->where('location_id', $cooperator['cooperator_location_id'])->first(),
-			      'payroll_group' => $this->payrollGroupModel->where('pg_id', $cooperator['cooperator_payroll_group_id'])->first(),
-			      'bank' => $this->bankModel->where('bank_id', $cooperator['cooperator_bank_id'])->first(),
-			      'account_number' => $cooperator['cooperator_account_number'],
-			      'bank_branch' => $cooperator['cooperator_bank_branch'],
-			      'sort_code' => $cooperator['cooperator_sort_code'],
-			      'date' => $cooperator['cooperator_date'],
-			      'approved_date' => $cooperator['cooperator_approved_date'],
-			      'savings' => $cooperator['cooperator_savings'],
-			      'status' => $cooperator['cooperator_status'],
-			      'regular_savings' => $this->_get_regular_savings_amount($cooperator['cooperator_staff_id']),
-			      'savings_types_amounts_list' => $this->_get_savings_types_amounts($cooperator['cooperator_staff_id']),
-			      'active' => true
-		      );
-      		$this->session->set($user_data);
-		      $this->session->setFlashdata('login_success', 'You have logged in successfully!');
+            'dob' => $cooperator['cooperator_dob'],
+            'email' => $cooperator['cooperator_email'],
+            'address' => $cooperator['cooperator_address'],
+            'city' => $cooperator['cooperator_city'],
+            'state' => $this->stateModel->where('state_id', $cooperator['cooperator_state_id'])->first(),
+            'department' => $this->departmentModel->where('department_id', $cooperator['cooperator_department_id'])->first(),
+            'location' => $this->locationModel->where('location_id', $cooperator['cooperator_location_id'])->first(),
+            'payroll_group' => $this->payrollGroupModel->where('pg_id', $cooperator['cooperator_payroll_group_id'])->first(),
+            'bank' => $this->bankModel->where('bank_id', $cooperator['cooperator_bank_id'])->first(),
+            'account_number' => $cooperator['cooperator_account_number'],
+            'bank_branch' => $cooperator['cooperator_bank_branch'],
+            'sort_code' => $cooperator['cooperator_sort_code'],
+            'date' => $cooperator['cooperator_date'],
+            'approved_date' => $cooperator['cooperator_approved_date'],
+            'savings' => $cooperator['cooperator_savings'],
+            'status' => $cooperator['cooperator_status'],
+            'regular_savings' => $this->_get_regular_savings_amount($cooperator['cooperator_staff_id']),
+            'savings_types_amounts_list' => $this->_get_savings_types_amounts($cooperator['cooperator_staff_id']),
+            'active' => true
+          );
+          $this->session->set($user_data);
+          $this->session->setFlashdata('login_success', 'You have logged in successfully!');
 //		      print_r($this->_get_savings_types_amounts($user_data['staff_id']));
-		      return redirect('dashboard');
-	      }
+          return redirect('dashboard');
+        }
       } else {
-      	print_r('Not Found');
+        $this->session->setFlashdata('login_failure', 'Invalid login credentials!');
+        return redirect('auth/login');
       }
     }
   }
 
-  function logout() {
-  	if ($this->session->active) {
-  		$this->session->stop();
-  		$this->session->destroy();
-	  }
-  	return redirect('auth/login');
+  function logout()
+  {
+    if ($this->session->active) {
+      $this->session->stop();
+      $this->session->destroy();
+    }
+    return redirect('auth/login');
   }
 
-  private function _get_savings_types_amounts($staff_id): array {
+  private function _get_savings_types_amounts($staff_id): array
+  {
     $savings_types = $this->_get_savings_types($staff_id);
     $savings_types_amounts = array();
     foreach ($savings_types as $savings_type) {
--- app/Views/auth/login.php
+++ app/Views/auth/login.php
@@ -1,87 +1,67 @@
 <!DOCTYPE html>
 <html lang="zxx" class="js">
-<?php include(APPPATH.'/Views/_head.php'); ?>
+<?php include(APPPATH . '/Views/_head.php'); ?>
 <body class="nk-body npc-crypto bg-white pg-auth">
 <!-- app body @s -->
 <div class="nk-app-root">
   <div class="nk-split nk-split-page nk-split-md">
     <div class="nk-split-content nk-block-area nk-block-area-column nk-auth-container bg-white">
       <div class="absolute-top-right d-lg-none p-3 p-sm-5">
-        <a href="#" class="toggle btn-white btn btn-icon btn-light" data-target="athPromo"><em class="icon ni ni-info"></em></a>
+        <a href="#" class="toggle btn-white btn btn-icon btn-light" data-target="athPromo"><em
+            class="icon ni ni-info"></em></a>
       </div>
       <div class="nk-block nk-block-middle nk-auth-body">
         <div class="brand-logo pb-4 ml-0">
           <a href="/" class="logo-link ml-n5">
-            <img class="logo-light logo-img logo-img-lg" src="/assets/images/justice-logo.png" alt="logo">
-            <img class="logo-dark ml-n4" style="width: 35% !important; max-height: 1000px !important;" src="/assets/images/slb-logo.jpg" alt="logo-dark">
+            <img class="logo-light logo-img logo-img-lg" src="/assets/images/slb-logo.jpg" alt="logo">
+            <img class="logo-dark ml-n4" style="width: 35% !important; max-height: 1000px !important;"
+                 src="/assets/images/slb-logo.jpg" alt="logo-dark">
           </a>
         </div>
         <div class="nk-block-head">
           <div class="nk-block-head-content">
             <h5 class="nk-block-title">Login</h5>
             <div class="nk-block-des">
-              <p>Access the Member's Portal using your Staff ID and Password.</p>
+              <p>Access the Member's Portal using your credentials.</p>
             </div>
           </div>
         </div><!-- .nk-block-head -->
-        <form class="form-validate" action="<?= site_url('login')?>" method="post">
+        <form class="form-validate" action="<?= site_url('login') ?>" method="post">
           <div class="form-group">
             <div class="form-label-group">
               <label class="form-label" for="default-01">Staff ID</label>
-              <a class="link link-primary link-sm" tabindex="-1" href="#">Need Help?</a>
             </div>
-            <input autocomplete="off" type="text" class="form-control form-control-lg" id="default-01" name="staff_id" placeholder="Enter your staff id" required>
+            <input autocomplete="off" type="text" class="form-control form-control-lg" id="default-01" name="staff_id"
+                   placeholder="Enter your staff id" required>
           </div><!-- .form-group -->
           <div class="form-group">
             <div class="form-label-group">
               <label class="form-label" for="password">Password</label>
-              <a class="link link-primary link-sm" tabindex="-1" href="#">Forgot Code?</a>
+              <!--              <a class="link link-primary link-sm" tabindex="-1" href="#">Forgot Code?</a>-->
             </div>
             <div class="form-control-wrap">
               <a tabindex="-1" href="#" class="form-icon form-icon-right passcode-switch" data-target="password">
                 <em class="passcode-icon icon-show icon ni ni-eye"></em>
                 <em class="passcode-icon icon-hide icon ni ni-eye-off"></em>
               </a>
-              <input autocomplete="off" type="password" class="form-control form-control-lg" id="password" name="password" placeholder="Enter your passcode" required>
+              <input autocomplete="off" type="password" class="form-control form-control-lg" id="password"
+                     name="password" placeholder="Enter your passcode" required>
             </div>
           </div><!-- .foem-group -->
           <div class="form-group">
+            <input type="hidden" class="invalid-login"/>
             <button class="btn btn-lg btn-primary btn-block">Login</button>
           </div>
         </form><!-- form -->
-<!--        <div class="form-note-s2 pt-4"> New on our platform? <a href="html/pages/auths/auth-register.html">Create an account</a>-->
-<!--        </div>-->
-<!--        <div class="text-center pt-4 pb-3">-->
-<!--          <h6 class="overline-title overline-title-sap"><span>OR</span></h6>-->
-<!--        </div>-->
-<!--        <ul class="nav justify-center gx-4">-->
-<!--          <li class="nav-item"><a class="nav-link" href="#">Facebook</a></li>-->
-<!--          <li class="nav-item"><a class="nav-link" href="#">Google</a></li>-->
-<!--        </ul>-->
-<!--        <div class="text-center mt-5">-->
-<!--          <span class="fw-500">I don't have an account? <a href="#">Try 15 days free</a></span>-->
-<!--        </div>-->
+        <div class="form-note-s2 pt-4"> New on our platform? <a href="/">Download the membership form</a>
+        </div>
       </div><!-- .nk-block -->
       <div class="nk-block nk-auth-footer">
-        <div class="nk-block-between">
-          <ul class="nav nav-sm">
-            <li class="nav-item">
-              <a class="nav-link" href="#">Terms & Condition</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Privacy Policy</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Help</a>
-            </li>
-          </ul><!-- .nav -->
-        </div>
         <div class="mt-3">
-          <p>&copy; <?=date('Y')?> Connexxion Telecom. All Rights Reserved.</p>
+          <p>&copy; <?= date('Y') ?> Connexxion Telecom. All Rights Reserved.</p>
         </div>
       </div><!-- .nk-block -->
     </div><!-- .nk-split-content -->
-<!--    <div class="">-->
     <div
       class="nk-split-content nk-split-stretch bg-abstract"
       style="background: url('/assets/images/login-1.jpg') no-repeat center center fixed;
@@ -94,7 +74,12 @@
   </div><!-- .nk-split -->
 </div><!-- app body @e -->
 <!-- JavaScript -->
-<?php include(APPPATH.'/Views/_scripts.php'); ?>
+<?php include(APPPATH . '/Views/_scripts.php'); ?>
+<?php if (session()->getFlashdata('login_failure') !== NULL) : ?>
+  <script>
+    $('.invalid-login').click()
+  </script>
+<?php endif; ?>
 </body>
 
 </html>
